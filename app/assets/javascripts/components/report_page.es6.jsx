27a27e1c3c74ab204fd5cc4066e27f6cff907a33
Eric Liang <liang.eric24@gmail.com>
2016-04-09 08:29:31 -0700

    styled report page

--- app/assets/javascripts/components/report_page.es6.jsx
+++ app/assets/javascripts/components/report_page.es6.jsx
@@ -13,10 +13,31 @@ class ReportPage extends React.Component {
   // Render
   // --------------------------------------------------
   render() {
+    var mapURL = `https://www.google.com/maps/embed/v1/place?q=${this.props.report.latitude},${this.props.report.longitude}&zoom=13&key=AIzaSyALPMkFwJP8jUyMlIlsd-f1cv41Hb6Xepg`
+    console.log(this.props.report)
     return (
       <div className="report-page">
         <Header />
-        <p>{this.props.report.description}</p>
+        <div className="container report-container">
+          <div className="col-md-5">
+            <ul className="quick-facts-container">
+              <div className={`severity-container severe-${this.props.report.severity}`}></div>
+              <li className="fact">
+                <div className="fact-title"><span className="fa fa-compass"></span>Location</div>
+                <div className="fact-data">{this.props.report.location}</div>
+              </li>
+              <li className="fact">
+                <div className="fact-title"><span className="fa fa-compass"></span>Location</div>
+                <div className="fact-data">{this.props.report.location}</div>
+              </li>
+            </ul>
+            <iframe className="report-map" src={mapURL}></iframe>
+          </div>
+          <div className="col-md-7">
+            <h1 className="report-title">{this.props.report.title}</h1>
+            <p className="report-description">{this.props.report.description}</p>
+          </div>
+        </div>
       </div>
     );
   }
