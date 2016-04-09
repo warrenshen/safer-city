d04cf4d2bf249445ff62ebbffd49ac454fab36f2
warrenshen <warrenzshen@gmail.com>
2016-04-09 01:57:21 -0700

    Automatically seed xlsx if one exists in db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
 Category.names.each do |name|
   Category.find_or_create_by(name: name.downcase)
 end
+
+if File.exist?('safecity-overall.xlsx')
+  importer = ImportService.new('safecity-overall.xlsx')
+  importer.parse_file
+end
