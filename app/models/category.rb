fb254af31cf5e9bae390b2adddf3ecce8663f514
Howard Chen <howardxchen@gmail.com>
2016-04-08 21:21:40 -0700

    Create report and category model

---
+++ app/models/category.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Category < ActiveRecord::Base
+  has_many :report_categories
+  has_many :reports, through: :report_categories
+end
---
+++ db/schema.rb
@@ -0,0 +1,60 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20160409040324) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "report_categories", force: :cascade do |t|
+    t.integer  "report_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "report_categories", ["category_id"], name: "index_report_categories_on_category_id", using: :btree
+  add_index "report_categories", ["report_id"], name: "index_report_categories_on_report_id", using: :btree
+
+  create_table "reports", force: :cascade do |t|
+    t.integer  "incident_id"
+    t.date     "date"
+    t.time     "time"
+    t.text     "description"
+    t.string   "location"
+    t.integer  "country_code"
+    t.integer  "city_code"
+    t.integer  "area_code"
+    t.integer  "locatlity"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.boolean  "confirm"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.text     "more_info"
+    t.string   "email"
+    t.boolean  "approved"
+    t.boolean  "verified"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_foreign_key "report_categories", "categories"
+  add_foreign_key "report_categories", "reports"
+end
