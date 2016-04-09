d57cec9e7eb348689f8af292fc36eb04ebf13fab
Howard Chen <howardxchen@gmail.com>
2016-04-08 23:48:26 -0700

    Seeds without time info

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160409040324) do
+ActiveRecord::Schema.define(version: 20160409052256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,8 +51,10 @@ ActiveRecord::Schema.define(version: 20160409040324) do
     t.string   "email"
     t.boolean  "approved"
     t.boolean  "verified"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.string   "title"
+    t.integer  "locatlity_code"
   end
 
   add_foreign_key "report_categories", "categories"
