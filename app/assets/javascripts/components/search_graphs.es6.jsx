38f71403a944e305272f74769a468ca09821fde3
Sameera Vemulapalli <sameera.vemulapalli@berkeley.edu>
2016-04-08 23:42:53 -0700

    stuff

--- app/assets/javascripts/components/search_graphs.es6.jsx
+++ app/assets/javascripts/components/search_graphs.es6.jsx
@@ -1,28 +1,131 @@
+window.initAutocomplete = function() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -33.8688, lng: 151.2195},
+    zoom: 13,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  var markers = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+}
+
 class SearchGraphs extends React.Component {
 
   // --------------------------------------------------
   // Lifecycle
   // --------------------------------------------------
   componentDidMount() {
-    var margin = {top: 100, right: 100, bottom: 100, left: 100},
-        width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
 
-    var x = d3.time.scale()
-        .domain([new Date, new Date])
-        .nice(d3.time.week)
-        .range([0, width]);
+    // PIE CHART
+    var dataset = [
+      { label: 'Abulia', count: 10 }, 
+      { label: 'Betelgeuse', count: 20 },
+      { label: 'Cantaloupe', count: 30 },
+      { label: 'Dijkstra', count: 40 }
+    ];
 
-    var svg = d3.select("#d3").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    var width = 360;
+    var height = 360;
+    var radius = Math.min(width, height) / 2;    
 
-    svg.append("g")
-        .attr("class", "x axis")
-        .call(d3.svg.axis().scale(x).orient("bottom"));
+    var color = d3.scale.category20b();
+    
+    var svg = d3.select('#d3-pie-chart')
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height)
+      .append('g')
+      .attr('transform', 'translate(' + (width / 2) +  ',' + (height / 2) + ')');
+
+    var arc = d3.svg.arc()
+      .outerRadius(radius);
+
+    var pie = d3.layout.pie()
+      .value(function(d) { return d.count; })
+      .sort(null);
+
+    var path = svg.selectAll('path')
+      .data(pie(dataset))
+      .enter()
+      .append('path')
+      .attr('d', arc)
+      .attr('fill', function(d, i) { 
+        return color(d.data.label);
+      });
+
+    var ages = [30, 22, 33, 45];
+    var totalWidth = 500;
+    var totalHeight = 200;
+    var scale = {
+      y: d3.scale.linear()
+    };
+    scale.y.domain([0, 100]);
+    scale.y.range([0, 200]);
+    var chart = d3.select('#d3-pbar-graph')
+    .attr({
+        'width': totalWidth,
+        'height': totalHeight
+    });
+
+    var bars = chart
+        .selectAll('g')
+        .data(ages)
+        .enter()
+        .append('g');
   }
+
 
   // --------------------------------------------------
   // Render
@@ -30,7 +133,10 @@ class SearchGraphs extends React.Component {
   render() {
     // TODO(Sameera): Place any d3 component inside the divs below.
     return (
-      <div className="d3-graph" id="d3">
+      <div>
+        <input id="pac-input" className="controls" type="text" placeholder="Search Box" />
+        <div id="map"></div>
+        <div id="d3-pie-chart"></div>
       </div>
     );
   }
