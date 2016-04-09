77a84882466bc89d211250741bbad75b5ac72c8d
warrenshen <warrenzshen@gmail.com>
2016-04-09 00:02:03 -0700

    Include xlsx in git ignore and syntax fixes in import service

--- app/services/import_service.rb
+++ app/services/import_service.rb
@@ -1,4 +1,5 @@
 class ImportService
+
   def initialize(file)
     @xlsx = Roo::Spreadsheet.open(file, extension: :xlsx)
   end
@@ -30,7 +31,7 @@ class ImportService
         last_name: row[40],
         email: row[41],
         approved: row[42] == 'YES',
-        more_info: row[43] == 'YES'
+        more_info: row[43] == 'YES',
       )
     end
   end
@@ -42,7 +43,7 @@ class ImportService
         categories << Category.find_by_name(Category.names[i-12].downcase())
       end
     end
-    puts categories.count
     categories
   end
+
 end
